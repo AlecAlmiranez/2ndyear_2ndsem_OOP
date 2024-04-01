2b5d5e0f09ddf5434de6f7b1b0bef20bd362d401
AlecAlmiranez <165567578+AlecAlmiranez@users.noreply.github.com>
2024-04-01 09:56:02 +0800

    Added Employee, Account, Task classes

---
+++ Finals/Employee.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Employee {
+    private String Name;
+    private int ENum;
+    private ArrayList<String> Projects;   
+}
---
+++ Finals/Task.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Task {
+    private String Title;
+    private String Description;
+    private int Deadline;
+    private String Status;   
+}
